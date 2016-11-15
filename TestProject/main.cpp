409bee273e9de7ca9175077f05697421b95d0d99
nas5690 <nsiddiqui590@gmail.com>
2016-11-14 21:09:23 -0600

    back to original code

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int x, y;
-
 int main()
 {
 	cout << "Attempting at trying to GitHub" << endl;
 	cout << "Hopefully it's working." << endl;
 	cout << "Awesome test project." << endl;
-
-	cout << "Addition Problem." << endl;
-	cout << "Enter value for x: " << endl;
-	cin >> x;
-	cout << "Enter value for y: " << endl;
-	cin >> y;
-
-	cout << "x + y = " << x + y << endl;
-
 
 	system("PAUSE");
 	return 0;
